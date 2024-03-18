75770d89550398bd069ce24ba84bcb81e706513c
Deepraj <mail30deeprajdas.com>
2024-03-18 16:42:27 +0530

    refactored otp function

--- authentication/services/email/send_otp.py
+++ authentication/services/email/send_otp.py
@@ -1,7 +1,14 @@
 import smtplib
 from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", "..", ".env")
+load_dotenv(dotenv_path=dotenv_path)
 
 def send_otp(recipient):
+    smtp_server = os.getenv("SMTP_SERVER")
+    smtp_port = os.getenv("SMTP_PORT")
+    smtp_password = os.getenv("SMTP_PASSWORD")
     sender_email = "@gmail.com"
     message = EmailMessage()
     message.set_content("Hello")
@@ -9,9 +16,9 @@ def send_otp(recipient):
     message["From"] = sender_email
     message["To"] = recipient
     
-    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    with smtplib.SMTP(smtp_server, int(smtp_port)) as smtp:
         smtp.starttls()  # Enable TLS
-        smtp.login(sender_email, "")
+        smtp.login(sender_email, smtp_password)
         smtp.send_message(message)
 
 send_otp("@gmail.com")
